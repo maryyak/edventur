fc2d6427a4a2bafdd9ad21acf4afa700e33a4769
Таня <tanya.savenkova2005@gmail.com>
2025-04-15 01:58:32 +0300

    Реализация выхода из профиля, отображение кнопки входа при неавторизированном пользователе

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import {Badge, Dropdown, Flex, Layout, Menu} from "antd";
+import {Badge, Button, Dropdown, Flex, Layout, Menu} from "antd";
 import {
     ArrowRightOutlined,
     BellOutlined, UserOutlined
 } from "@ant-design/icons";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {notifications} from "../utils/mock";
 import {useUserInfo} from "../context/UserInfoContext";
 import useAuth from "../hooks/api/users/authUser";
 
-//тут надо еще добавить механизм выхода из аккаунта
 const Header = () => {
     const { userInfo } = useUserInfo();
     const { logout } = useAuth();
+    const navigate = useNavigate();
     const menu = (
         <Menu>
             {userInfo && userInfo?.username ? (
-                // Меню для авторизованного пользователя
                 <>
                     <Menu.Item key="account">
                         <Link to="/settings">
@@ -32,7 +31,6 @@ const Header = () => {
                     </Menu.Item>
                 </>
             ) : (
-                // Меню для неавторизованного пользователя
                 <Menu.Item key="login" style={{backgroundColor: " #318d25", color: "#fff"}} >
                     <Link to="/login">Войти</Link>
                 </Menu.Item>
@@ -42,16 +40,26 @@ const Header = () => {
 
     return (
         <Layout.Header>
-            <Flex gap={20} justify="flex-end" style={{marginRight: 103, marginTop: 10}}>
-                <Link to={'/'}>
-                    <Badge count={notifications.length} color="#08a652">
-                        <BellOutlined style={{fontSize: 24, color: 'gray'}}/>
-                    </Badge>
-                </Link>
-                <Dropdown overlay={menu} trigger={['hover']} placement="bottomCenter">
-                    <UserOutlined style={{fontSize: 24, color: "#08a652", cursor: 'pointer'}}/>
-                </Dropdown>
-            </Flex>
+            {userInfo && userInfo?.username ? (
+                <>
+                    <Flex gap={20} justify="flex-end" style={{marginRight: 103, marginTop: 10}}>
+                        <Link to={'/'}>
+                            <Badge count={notifications.length} color="#08a652">
+                                <BellOutlined style={{fontSize: 24, color: 'gray'}}/>
+                            </Badge>
+                        </Link>
+                        <Dropdown overlay={menu} trigger={['hover']} placement="bottomCenter">
+                            <UserOutlined style={{fontSize: 24, color: "#08a652", cursor: 'pointer'}}/>
+                        </Dropdown>
+                    </Flex>
+                </>
+            ) : (
+                <Flex gap={20} justify="flex-end" style={{marginRight: 103, marginTop: 10}}>
+                    <Button type="primary" onClick={() => navigate('/login')}>
+                        Войти
+                    </Button>
+                </Flex>
+            )}
         </Layout.Header>
     );
 };
